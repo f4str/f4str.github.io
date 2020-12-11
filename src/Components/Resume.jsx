9158052f27ae4ec0a306825d92f59796e78f2483
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-12-10 21:55:12 -0500

    renamed js files to jsx

---
+++ src/Components/Resume.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+	render() {
+		return (
+			<section id="resume">
+				<div className="row education">
+					<div className="three columns header-col">
+						<h1><span>Education</span></h1>
+					</div>
+					<div className="nine columns main-col">
+						<div key="Stony Brook University">
+							<h2 className="school">Stony Brook University</h2>
+							<h3 className="degree">Bachelors in Computer Science &#38; Applied Mathematics</h3>
+							<div>
+								<span className="date">Expected May 2021</span>
+								<span className="gpa">GPA: 3.95/4.0</span>
+							</div>
+							<p className="info">
+								Computer Science Honors Program
+							</p>
+						</div>
+					</div>
+				</div>
+				
+				<div className="row internship">
+					<div className="three columns header-col">
+						<h1><span>Internships</span></h1>
+					</div>
+					<div className="nine columns main-col">
+						<div key="Barclays">
+							<h2 className="company">Barclays</h2>
+							<h3 className="role">Software Engineer Intern</h3>
+							<p className="date">June 2020 - August 2020</p>
+							<p className="newline description"></p>
+						</div>
+						<div key="Stony Brook University">
+							<h2 className="company">Stony Brook University</h2>
+							<h3 className="role">Software Engineer Intern</h3>
+							<p className="date">September 2018 - May 2020</p>
+							<p className="newline description"></p>
+						</div>
+						<div key="Peleton">
+							<h2 className="company">Peloton</h2>
+							<h3 className="role">Data Science Intern</h3>
+							<p className="date">June 2019 - August 2019</p>
+							<p className="newline description"></p>
+						</div>
+						<div key="Bloomberg">
+							<h2 className="company">Lighthouse Guild</h2>
+							<h3 className="role">Software Engineer Intern</h3>
+							<p className="date">June 2018 - August 2018</p>
+							<p className="newline description"></p>
+						</div>
+						<div key="Bloomberg">
+							<h2 className="company">Bloomberg</h2>
+							<h3 className="role">Software Engineer Intern</h3>
+							<p className="date">July 2017 - August 2017</p>
+							<p className="newline description"></p>
+						</div>
+					</div>
+				</div>
+				
+				<div className="row research">
+					<div className="three columns header-col">
+						<h1><span>Research</span></h1>
+					</div>
+					<div className="nine columns main-col">
+						<div key="HLAB Lab">
+							<h2 className="company">HLAB Lab</h2>
+							<h3 className="role">Research Assistant</h3>
+							<p className="date">June 2020 - Present</p>
+							<p className="newline description"></p>
+						</div>
+						<div key="Jefferson Lab">
+							<h2 className="company">Ethos Lab</h2>
+							<h3 className="role">Research Assistant</h3>
+							<p className="date">September 2019 - Present</p>
+							<p className="newline description"></p>
+						</div>
+						<div key="Jefferson Lab">
+							<h2 className="company">Jefferson Lab</h2>
+							<h3 className="role">Research Assistant</h3>
+							<p className="date">January 2018 - August 2018</p>
+							<p className="newline description"></p>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Resume;
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Skills extends Component {
+	render() {
+		return (
+			<section id="skills">
+				<div className="row skill">
+					<div className="header-col center">
+						<h1><span>My Favorite Toolkits</span></h1>
+					</div>
+					<div>
+						<ul className="bgrid-thirds s-bgrid-thirds cf">
+							<div key="Python" className="columns feature-item">
+								<h2>Python</h2>
+								<img className='skill' alt="Python" src='images/python.png' />
+							</div>
+							<div key="PyTorch" className="columns feature-item">
+								<h2>PyTorch</h2>
+								<img className='skill' alt="PyTorch" src='images/pytorch.png' />
+							</div>
+							<div key="TensorFlow" className="columns feature-item">
+								<h2>TensorFlow</h2>
+								<img className='skill' alt="TensorFlow" src='images/tensorflow.png' />
+							</div>
+							<div key="C" className="columns feature-item">
+								<h2>C</h2>
+								<img className='skill' alt="C" src='images/c.png' />
+							</div>
+							<div key="Vim" className="columns feature-item">
+								<h2>Vim</h2>
+								<img className='skill' alt="Vim" src='images/vim.png' />
+							</div>
+							<div key="CSS" className="columns feature-item">
+								<h2>Linux</h2>
+								<img className='skill' alt="Linux" src='images/linux.png' />
+							</div>
+							<div key="CSharp" className="columns feature-item">
+								<h2>C&#35;</h2>
+								<img className='skill' alt="CSharp" src='images/csharp.png' />
+							</div>
+							<div key="JavaScript" className="columns feature-item">
+								<h2>JavaScript</h2>
+								<img className='skill' alt="JavaScript" src='images/javascript.png' />
+							</div>
+							<div key="Git" className="columns feature-item">
+								<h2>Git</h2>
+								<img className='skill' alt="Git" src='images/git.png' />
+							</div>
+						</ul>
+					</div>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Skills;
