280ea68c3cf211b11801fc0411a79bd85033ecb6
Farhan Ahmed <farhaahmed@cs.stonybrook.edu>
2022-03-17 18:38:31 -0400

    updated formatting

--- src/Components/Publications.jsx
+++ src/Components/Publications.jsx
@@ -10,9 +10,18 @@ function Publications() {
 					<h1>Publications</h1>
 				</div>
 				<div className="main">
+					<div key="ares">
+						<div className="paper">
+							Ares: A System-Oriented Wargame Framework for Adversarial ML
+						</div>
+						<div className='authors'>
+							<span className='me'>Farhan Ahmed</span>, Pratik Vaishnavi, Kevin Ekyholt, and Amir Rahmati. 
+							The Fifth Deep Learning and Security Workshop at IEEE (DLS) 2022.
+						</div>
+					</div>
 					<div key="correcting">
 						<div className="paper">
-							Correcting Sociodemographic Selection Biases for Population Prediction from Social Media.
+							Correcting Sociodemographic Selection Biases for Population Prediction from Social Media
 						</div>
 						<div className='authors'>
 							Salvatore Giorgi, Veronica Lynn, Keshav Gupta, <span className='me'>Farhan Ahmed</span>, 
@@ -22,14 +31,14 @@ function Publications() {
 					</div>
 					<div key="human-centered">
 						<div className="paper">
-							A Human-Centered Hierarchical Framework for Dialogue System Construction and Evaluation.
+							A Human-Centered Hierarchical Framework for Dialogue System Construction and Evaluation
 						</div>
 						<div className='authors'>
 							Salvatore Giorgi, <span className='me'>Farhan Ahmed</span>, Lyle Ungar, and H. Andrew Schwartz.
 							The Tenth Dialog System Technology Challenge at AAAI (DSTC10) 2022.
 						</div>
 					</div>
-					<div key="optimized">
+					{/* <div key="optimized">
 						<div className="paper">
 							Optimized Neural Post-stratification for Twitter-based County Health Estimation
 						</div>
@@ -37,17 +46,8 @@ function Publications() {
 							<span className='me'>Farhan Ahmed</span>, Salvatore Giorgi, Lyle Ungar, and H. Andrew Schwartz.
 							International Conference on Web and Social Media (ICWSM) 2022. [Under review]
 						</div>
-					</div>
-					<div key="ares">
-						<div className="paper">
-							Ares: A System-Oriented Wargame Framework for Adversarial ML
-						</div>
-						<div className='authors'>
-							<span className='me'>Farhan Ahmed</span>, Pratik Vaishnavi, Kevin Ekyholt, and Amir Rahmati. 
-							The Fifth Deep Learning and Security Workshop at IEEE (DLS) 2022. [Under review]
-						</div>
-					</div>
-					<div key="robust">
+					</div> */}
+					{/* <div key="robust">
 						<div className="paper">
 							Robust Image Classification using Edge Features
 						</div>
@@ -55,7 +55,7 @@ function Publications() {
 							Pratik Vaishnavi, <span className='me'>Farhan Ahmed</span>, Veena Krish, Kevin Ekyholt, and Amir Rahmati. 
 							ACM ASIA Conference on Computer and Communications Security (AsiaCCS) 2022. [Under review]
 						</div>
-					</div>
+					</div> */}
 				</div>
 			</div>
 		</section>
--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
 				</div>
 				<div className="nine columns main-col">
 					<div key="Stony Brook University G">
-						<h2 className="school">Stony Brook University | Stony Brook, NY</h2>
+						<h3 className="school">Stony Brook University | Stony Brook, NY</h3>
 						<h3 className="degree">Master of Science in Computer Science</h3>
 						<div>
 							<span className="date">August 2021 &#8211; May 2022</span>
@@ -20,7 +20,7 @@ function Resume() {
 						</p>
 					</div>
 					<div key="Stony Brook University UG">
-						<h2 className="school">Stony Brook University | Stony Brook, NY</h2>
+						<h3 className="school">Stony Brook University | Stony Brook, NY</h3>
 						<h3 className="degree">Bachelor of Science in Computer Science &#38; Applied Mathematics</h3>
 						<div>
 							<span className="date">August 2017 &#8211; May 2021</span>
@@ -39,37 +39,31 @@ function Resume() {
 				</div>
 				<div className="nine columns main-col">
 					<div key="Kitware">
-						<h2 className="company">Kitware | Clifton Park, NY</h2>
+						<h3 className="company">Kitware | Clifton Park, NY</h3>
 						<h3 className="role">Research &#38; Development Intern</h3>
 						<p className="date">June 2021 &#8211; August 2021</p>
 						<p className="newline description"></p>
 					</div>
 					<div key="Barclays">
-						<h2 className="company">Barclays | New York, NY</h2>
+						<h3 className="company">Barclays | New York, NY</h3>
 						<h3 className="role">Software Engineer Intern</h3>
 						<p className="date">June 2020 &#8211; August 2020</p>
 						<p className="newline description"></p>
 					</div>
-					{/* <div key="Stony Brook University">
-						<h2 className="company">Stony Brook University</h2>
-						<h3 className="role">Software Engineer Intern</h3>
-						<p className="date">September 2018 &#8211; May 2020</p>
-						<p className="newline description"></p>
-					</div> */}
 					<div key="Peleton">
-						<h2 className="company">Peloton | New York, NY</h2>
+						<h3 className="company">Peloton | New York, NY</h3>
 						<h3 className="role">Data Science Intern</h3>
 						<p className="date">June 2019 &#8211; August 2019</p>
 						<p className="newline description"></p>
 					</div>
 					<div key="Bloomberg">
-						<h2 className="company">Bloomberg | New York, NY</h2>
+						<h3 className="company">Bloomberg | New York, NY</h3>
 						<h3 className="role">Software Engineer Intern</h3>
 						<p className="date">July 2018 &#8211; August 2018</p>
 						<p className="newline description"></p>
 					</div>
 					<div key="Lighthouse Guild">
-						<h2 className="company">Lighthouse Guild | New York, NY</h2>
+						<h3 className="company">Lighthouse Guild | New York, NY</h3>
 						<h3 className="role">Software Engineer Intern</h3>
 						<p className="date">June 2017 &#8211; August 2017</p>
 						<p className="newline description"></p>
@@ -83,19 +77,19 @@ function Resume() {
 				</div>
 				<div className="nine columns main-col">
 					<div key="HLAB Lab">
-						<h2 className="company">HLAB Lab | Stony Brook, NY</h2>
+						<h3 className="company">Human Language Analysis Beings Lab | Stony Brook, NY</h3>
 						<h3 className="role">Research Assistant</h3>
 						<p className="date">June 2020 &#8211; Present</p>
 						<p className="newline description"></p>
 					</div>
-					<div key="Jefferson Lab">
-						<h2 className="company">Ethos Lab | Stony Brook, NY</h2>
+					<div key="Ethos Lab">
+						<h3 className="company">Ethos Lab | Stony Brook, NY</h3>
 						<h3 className="role">Research Assistant</h3>
 						<p className="date">September 2019 &#8211; Present</p>
 						<p className="newline description"></p>
 					</div>
 					<div key="Jefferson Lab">
-						<h2 className="company">Jefferson Lab | Stony Brook, NY</h2>
+						<h3 className="company">Jefferson Lab | Stony Brook, NY</h3>
 						<h3 className="role">Research Assistant</h3>
 						<p className="date">January 2018 &#8211; August 2018</p>
 						<p className="newline description"></p>
